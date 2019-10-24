54190ca9f1468ba912d284f0d5fd2e895b4b86a8
chzeze <chzeze@qq.com>
2019-10-24 17:42:34 +0800

    fix(cz): Test cz git

--- src/main/java/com/jourwon/httpclient/Application.java
+++ src/main/java/com/jourwon/httpclient/Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Description: springboot启动类
  * 
- * @author JourWon
- * @date Created on 2018年4月19日
  */
 @SpringBootApplication
 public class Application {
